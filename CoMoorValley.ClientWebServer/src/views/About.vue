9d90274dea61baa9dedaae121dc508e1b1381722
Kristen Hauser <hausermkristen@gmail.com>
2018-07-14 14:51:29 -0400

    template about page

--- CoMoorValley.ClientWebServer/src/views/About.vue
+++ CoMoorValley.ClientWebServer/src/views/About.vue
@@ -3,103 +3,84 @@
     <page-header :title="title"></page-header>
 
     <section class="section">
-      <div class="content">
-        <h1>Hello World</h1>
-        <p>Lorem ipsum<sup><a>[1]</a></sup> dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices
-          eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce
-          erat odio, sollicitudin vel erat vel, interdum mattis neque. Sub<sub>script</sub> works as well!</p>
-        <h2>Second level</h2>
-        <p>Curabitur accumsan turpis pharetra <strong>augue tincidunt</strong> blandit. Quisque condimentum maximus mi,
-          sit amet commodo arcu rutrum id. Proin pretium urna vel cursus venenatis. Suspendisse potenti. Etiam mattis
-          sem rhoncus lacus dapibus facilisis. Donec at dignissim dui. Ut et neque nisl.</p>
-        <ul>
-          <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-          <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-          <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
-          <li>Ut non enim metus.</li>
-        </ul>
-        <h3>Third level</h3>
-        <p>Quisque ante lacus, malesuada ac auctor vitae, congue <a href="#">non ante</a>. Phasellus lacus ex, semper ac
-          tortor nec, fringilla condimentum orci. Fusce eu rutrum tellus.</p>
-        <ol>
-          <li>Donec blandit a lorem id convallis.</li>
-          <li>Cras gravida arcu at diam gravida gravida.</li>
-          <li>Integer in volutpat libero.</li>
-          <li>Donec a diam tellus.</li>
-          <li>Aenean nec tortor orci.</li>
-          <li>Quisque aliquam cursus urna, non bibendum massa viverra eget.</li>
-          <li>Vivamus maximus ultricies pulvinar.</li>
-        </ol>
-        <blockquote>Ut venenatis, nisl scelerisque sollicitudin fermentum, quam libero hendrerit ipsum, ut blandit est
-          tellus sit amet turpis.
-        </blockquote>
-        <p>Quisque at semper enim, eu hendrerit odio. Etiam auctor nisl et <em>justo sodales</em> elementum. Maecenas
-          ultrices lacus quis neque consectetur, et lobortis nisi molestie.</p>
-        <p>Sed sagittis enim ac tortor maximus rutrum. Nulla facilisi. Donec mattis vulputate risus in luctus. Maecenas
-          vestibulum interdum commodo.</p>
-        <dl>
-          <dt>Web</dt>
-          <dd>The part of the Internet that contains websites and web pages</dd>
-          <dt>HTML</dt>
-          <dd>A markup language for creating web pages</dd>
-          <dt>CSS</dt>
-          <dd>A technology to make HTML look better</dd>
-        </dl>
-        <p>Suspendisse egestas sapien non felis placerat elementum. Morbi tortor nisl, suscipit sed mi sit amet, mollis
-          malesuada nulla. Nulla facilisi. Nullam ac erat ante.</p>
-        <h4>Fourth level</h4>
-        <p>Nulla efficitur eleifend nisi, sit amet bibendum sapien fringilla ac. Mauris euismod metus a tellus laoreet,
-          at elementum ex efficitur.</p>
-        <pre>
-          &lt;!DOCTYPE html&gt;
-          &lt;html&gt;
-            &lt;head&gt;
-              &lt;title&gt;Hello World&lt;/title&gt;
-            &lt;/head&gt;
-            &lt;body&gt;
-              &lt;p&gt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec viverra nec nulla vitae mollis.&lt;/p&gt;
-            &lt;/body&gt;
-          &lt;/html&gt;
-        </pre>
-        <p>Maecenas eleifend sollicitudin dui, faucibus sollicitudin augue cursus non. Ut finibus eleifend arcu ut
-          vehicula. Mauris eu est maximus est porta condimentum in eu justo. Nulla id iaculis sapien.</p>
-        <table>
-          <thead>
-          <tr>
-            <th>One</th>
-            <th>Two</th>
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>Three</td>
-            <td>Four</td>
-          </tr>
-          <tr>
-            <td>Five</td>
-            <td>Six</td>
-          </tr>
-          <tr>
-            <td>Seven</td>
-            <td>Eight</td>
-          </tr>
-          <tr>
-            <td>Nine</td>
-            <td>Ten</td>
-          </tr>
-          <tr>
-            <td>Eleven</td>
-            <td>Twelve</td>
-          </tr>
-          </tbody>
-        </table>
+      <div class="tile is-ancestor">
+        <div class="tile is-parent is-8">
+          <article class="tile is-child box">
+            <p class="subtitle"><b>About CoMoor Valley</b></p>
+            <div class="content">
+              <p>{{introContent}}</p>
+            </div>
+          </article>
         </div>
+
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <img :src="rowOnePhoto" width="300" height="300" >
+          </article>
+        </div>
+      </div>
+
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <img :src="rowTwoPhoto" width="300" height="300" >
+          </article>
+        </div>
+        <div class="tile is-parent is-8">
+          <article class="tile is-child box">
+            <p class="subtitle"><b>History</b></p>
+            <div class="content">
+              <p>{{historyContent}}</p>
+            </div>
+          </article>
+        </div>
+      </div>
+
+      <div class="tile is-ancestor">
+        <div class="tile is-parent">
+          <article class="tile is-child box">
+            <p class="subtitle"><b>Board of Directors</b></p>
+            <div class="content">
+              <p>{{memberContent}}</p>
+
+              <div class="content grid">
+
+                <div v-for="(member, i) in boardMembers" :key="i" class="card cell">
+                  <div class="card-image">
+                    <figure class="image is-4by3">
+                      <img :src="member.src" alt="No image available.">
+                    </figure>
+                  </div>
+                  <div class="card-content">
+                    <div class="media">
+                      <div class="media-content">
+                        <p class="title is-4">{{member.name}}</p>
+                        <p class="subtitle is-6">{{member.title}}</p>
+                      </div>
+                    </div>
+
+                    <div class="content">
+                      {{member.description}}
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          </article>
+        </div>
+      </div>
+
     </section>
+
   </div>
 </template>
 
 <script>
 import PageHeader from '../components/PageHeader'
+import photo1 from '../assets/019.jpg'
+import photo2 from '../assets/060.jpg'
 
 export default {
   name: 'about',
@@ -108,11 +89,41 @@ export default {
   },
   data () {
     return {
-      title: 'About Us'
+      title: 'About',
+      rowOnePhoto: photo1,
+      rowTwoPhoto: photo2,
+      introContent: "Content topics here: Offerings such as pool, fitness center, etc? City info.",
+      historyContent: "Content topics here: When was it founded? Home many homes? Different types? Difference from CoMoor?",
+      memberContent: "Content topics here: Intro to how many board members, positions and general idea of what they do.",
+      boardMembers: [
+        {
+          name: "John Smith",
+          title: "President",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+          src: "https://bulma.io/images/placeholders/1280x960.png"
+        },
+        {
+          name: "Jane Smith",
+          title: "Board Member",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.",
+          src: "https://bulma.io/images/placeholders/1280x960.png"
+        }
+      ]
     }
   }
 }
 </script>
 
 <style>
+.grid {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.cell {
+  width: 33%;
+  color: var(--green-custom);
+  background-color: var(--off-white-custom);
+  margin: 5px;
+}
 </style>
